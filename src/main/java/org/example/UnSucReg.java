132855c499baa7c69273b914f190a37411925916
Rinonomy <maniaklovelyyy@gmail.com>
2024-09-25 18:56:14 +0300

    Add files via upload

---
+++ src/main/java/org/example/UnSucReg.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class UnSucReg {
+    private String error;
+
+    public String getError() {
+        return error;
+    }
+
+    public UnSucReg() {
+    }
+
+    public UnSucReg(String error) {
+        this.error = error;
+    }
+}
